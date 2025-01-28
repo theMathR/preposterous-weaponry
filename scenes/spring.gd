aa88330404e005757727e1bb0e8f31a1d2c3d6b5
theMathR <mathis.feraut@gmail.com>
2025-01-28 13:57:32 +0100

    Basic player sprites

---
+++ scenes/spring.gd
@@ -0,0 +1,8 @@
+@tool
+extends Sprite2D
+
+@export var to: Node2D
+
+func _process(delta):
+	global_rotation = global_position.angle_to_point(to.global_position) - PI/2
+	scale.y = global_position.distance_to(to.global_position)/texture.region.size.y
