1b6441ed99859d746b170eacf839f971a0d4e13f
theMathR <mathis.feraut@gmail.com>
2025-03-05 14:20:49 +0100

    Implemented Zapper upgrade

--- scripts/spring.gd
+++ scripts/spring.gd
@@ -4,5 +4,6 @@ extends Sprite2D
 @export var to: Node2D
 
 func _process(_delta):
+	if not to: return
 	global_rotation = global_position.angle_to_point(to.global_position) - PI/2 
-	scale.y = global_position.distance_to(to.global_position)/texture.region.size.y * $"../..".scale.x / $"..".scale.y
+	scale.y = $"..".global_scale.x * global_position.distance_to(to.global_position)/texture.region.size.y* sign( to.global_position.x - global_position.x)
--- scripts/upgrades/zapper.gd
+++ scripts/upgrades/zapper.gd
@@ -1,4 +1,23 @@
 extends GunPart
 
+var shooting = false
+
 func hit_target():
-	pass
+	var targets = $Area2D.get_overlapping_bodies().filter(func (x): return is_instance_of(x, Damageable))
+	
+	if not targets:
+		$Random.rotation_degrees = randf_range(-20, 20)
+		$Random/Node2D.position.x = randf_range(400, 800)
+		$Zap.to = $Random/Node2D
+	else:
+		var target = targets.pick_random()
+		hit(target)
+		$Zap.to = target
+
+func shoot():
+	shooting = true
+func release():
+	shooting = false
+
+func deploy():
+	$AnimationTree['parameters/conditions/deploy'] = true
